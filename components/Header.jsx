a0fa2b65e8f8e72b42cc53398f05f6dec743ecf4
rinideviani <deviani.rini@gmail.com>
2017-09-15 09:01:31 +0700

    Custom app bar and title

---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+import React, { PropTypes, Component } from 'react';
+import TodoTextInput from './TodoTextInput';
+
+import AppBar from 'material-ui/AppBar';
+
+const defaultStyle = {
+  marginLeft: 20
+};
+
+class Header extends Component {
+  handleSave(text) {
+    if (text.length !== 0) {
+      this.props.addTodo(text);
+    }
+  }
+
+  render() {
+    return (
+      <header className="header">
+          <AppBar title="RMS" />
+          <h1 style={defaultStyle} >Employee name</h1>
+          <TodoTextInput newTodo
+                         onSave={this.handleSave.bind(this)}
+                         placeholder="Add new employee" />
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  addTodo: PropTypes.func.isRequired
+};
+
+export default Header;
