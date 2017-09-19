2c5d6e9b07db361790a266a6d180d2ea56686348
rinideviani <deviani.rini@gmail.com>
2017-09-19 17:39:53 +0700

    Add drawer
    Add EmployeeDetailSection.jsx for drawer-like function

---
+++ components/EmployeeDetailSection.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+
+  class DrawerOpenRightExample extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  render() {
+    return (
+      <div>
+        
+        <Drawer width={900} openSecondary={true} open={this.state.open} >
+          <AppBar title="AppBar" />
+        </Drawer>
+      </div>
+    );
+  }
+}
+
+
+
+export default DrawerOpenRightExample;
